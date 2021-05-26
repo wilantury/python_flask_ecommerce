a8611c966b3b07ce5615f363b4334b1a058feede
Wilson Antury <ventas@antury.net>
2021-05-26 18:57:18 -0500

    [ADD] Home page styles - add bootstrap framework

--- market.py
+++ market.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<h1>Hello World hotreload</h1>"
+@app.route("/home")
+def home_page():
+    return render_template('home.html')
 
 @app.route("/about/<username>")
 def about_page(username):
