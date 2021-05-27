06eed2c80b8131ea061a1a36ab25dcacab22169a
Wilson Antury <ventas@antury.net>
2021-05-26 19:17:37 -0500

    [ADD] market html template

--- market.py
+++ market.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def home_page():
     return render_template('home.html')
 
+@app.route('/market')
+def market_page():
+    return render_template('market.html')
+
 @app.route("/about/<username>")
 def about_page(username):
     return f'<h2>This is about page of {username}</h2>'
