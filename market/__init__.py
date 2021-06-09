584629452a8b7f5ee52c85635bb61d328995d799
Wilson Antury <ventas@antury.net>
2021-06-09 12:19:48 -0500

    [ADD] Restructure project by using packages and circular imports

---
+++ market/__init__.py
@@ -0,0 +1,12 @@
+# flask
+from flask import Flask
+# third party
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+
+db = SQLAlchemy(app)
+
+# routes
+from market import routes
