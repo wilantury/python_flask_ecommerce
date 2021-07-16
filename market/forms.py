fc113cc168e9a0c0ba8a1ec639e8c97a9bcb0ff5
Wilson Antury <ventas@antury.net>
2021-07-16 17:07:58 -0500

    [ADD] bcrypt library to hash our plain text password, ans save it into DB, write the login html page

--- market/forms.py
+++ market/forms.py
@@ -20,3 +20,8 @@ class RegisterForm(FlaskForm):
     password1 = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
     password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1'), DataRequired()])
     submit = SubmitField(label='Create Account')
+
+class LoginForm(FlaskForm):
+    username = StringField(label='User name:', validators=[DataRequired()])
+    password = PasswordField(label='Password:', validators=[DataRequired()])
+    submit = SubmitField(label='Sign in')
