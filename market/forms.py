0e001dce6719eac44bb9dc3c7274330488c423ba
Wilson Antury <ventas@antury.net>
2021-07-16 16:09:48 -0500

    [ADD] flash errors into base template, add validations into Form class, rising Validation errors

--- market/forms.py
+++ market/forms.py
@@ -1,8 +1,20 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Length, EqualTo, Email, DataRequired
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from market.models import User
 
 class RegisterForm(FlaskForm):
+
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError('Username already exists, please try a different username')
+    
+    def validate_email_address(self, email_to_check):
+        user = User.query.filter_by(email_address=email_to_check.data).first()
+        if user:
+            raise ValidationError('Already exists an account with this email address')
+
     username = StringField(label='User Name:', validators=[Length(min=2, max=30), DataRequired()])
     email_address = StringField(label='Email Address:', validators=[Email(), DataRequired()])
     password1 = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
